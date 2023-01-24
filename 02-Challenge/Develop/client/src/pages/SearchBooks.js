edd87d82d294bcdac413881ef874a7889ba19245
Katie <ktmandovi@gmail.com>
2023-01-23 19:11:35 -0500

    use apollo useMutation function

--- 02-Challenge/Develop/client/src/pages/SearchBooks.js
+++ 02-Challenge/Develop/client/src/pages/SearchBooks.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import searchGoogleBooks from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -10,7 +10,6 @@ const SearchBooks = () => {
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
-
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
@@ -56,7 +55,7 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
+    const saveBook  = useMutation(SAVE_BOOK);
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
